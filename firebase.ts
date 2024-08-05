23e82b5bb45c9b108ce5def75747640bf2c99505
PhatTran_GitHub <141381198+mygithub2211@users.noreply.github.com>
2024-08-04 22:50:58 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, FirebaseApp } from "firebase/app"
-import { getAnalytics, Analytics } from "firebase/analytics"
-import { getFirestore, Firestore } from "firebase/firestore"
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getAnalytics, Analytics, isSupported } from "firebase/analytics";
+import { getFirestore, Firestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,11 +16,20 @@ const firebaseConfig = {
   messagingSenderId: "932033180492",
   appId: "1:932033180492:web:d3b05868d8c68a3c17d5e9",
   measurementId: "G-KHN603GVKX"
-}
+};
 
 // Initialize Firebase
-const app: FirebaseApp = initializeApp(firebaseConfig)
-const analytics: Analytics = getAnalytics(app)
-const firestore: Firestore = getFirestore(app)
+const app: FirebaseApp = initializeApp(firebaseConfig);
+let analytics: Analytics | null = null;
 
-export { firestore }
+if (typeof window !== 'undefined') {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+const firestore: Firestore = getFirestore(app);
+
+export { firestore, analytics };
